eeab06d0adae5cc7922849895b19422fa7181c9c
drehcodes <afilipe.lavrador@gmail.com>
2025-11-15 22:38:05 +0100

    fix curve to be smooth and added mid points

--- app.js
+++ app.js
@@ -5,25 +5,31 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 6;
+ctx.lineWidth = 4;
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+let last = {x: 0, y: 0}; //track current mouse
+let lastMid = {x: 0, y: 0}; //track end of the previous curve
 let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
 
-  const midX = (lastX + e.offsetX) / 2;
-  const midY = (lastY + e.offsetY) / 2;
+  // midpoint between the last real point and the current point
+  // gets end of the curve
+  const mid = {
+    x: (last.x + e.offsetX) / 2,
+    y: (last.y + e.offsetY) / 2,
+  };
 
   ctx.beginPath();
   ctx.strokeStyle = '#111';
-  ctx.moveTo(lastX, lastY) // Start from
-  ctx.quadraticCurveTo(midX, midY, e.offsetX, e.offsetY); // Move to
+  ctx.moveTo(lastMid.x, lastMid.y); // Start drawing from the previous mid point / curve
+  ctx.quadraticCurveTo(last.x, last.y, mid.x, mid.y); // Move to creating smooth arc
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+
+  last = {x: e.offsetX, y: e.offsetY}; // Update the last real mouse position to the current one
+  lastMid = mid; // Update the last midpoint to the current one
 
   if (ctx.lineWidth >= 6 || ctx.lineWidth <= 2) {
     direction = !direction;
@@ -35,7 +41,8 @@ function draw(e) {
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+  last = {x: e.offsetX, y: e.offsetY};
+  lastMid = {x: e.offsetX, y: e.offsetY};
 })
 
 canvas.addEventListener('mousemove', draw);
